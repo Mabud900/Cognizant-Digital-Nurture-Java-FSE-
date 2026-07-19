2ef6663911f9da2655800f47cc372b79fecfffe7
md mabud <mdmabud.mail56@gmail.com>
2026-07-20 01:02:28 +0530

    (Module 7 - Spring REST using Spring Boot 3)Hands On- 5:Exercise 5 - Generate token based on the user

--- Deepskilling/Module 7 - Spring REST using Spring Boot 3/Hands On 5 - JWT handson/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ Deepskilling/Module 7 - Spring REST using Spring Boot 3/Hands On 5 - JWT handson/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -1,11 +1,16 @@
 package com.cognizant.springlearn.controller;
 
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +25,9 @@ public class AuthenticationController {
         logger.debug("authHeader={}",authHeader);
 
         String user=getUser(authHeader);
+        String token = generateJwt(user);
         Map<String,String>map=new HashMap<String,String>();
-        map.put("token","");
+        map.put("token",token);
         logger.info("Authentication response sent");
         return map;
 
@@ -50,5 +56,13 @@ public class AuthenticationController {
         logger.debug("user={}", user);
 
         return user;
+    }
+    private String generateJwt(String user) {
+        JwtBuilder builder = Jwts.builder();
+        builder.setSubject(user);
+        builder.setIssuedAt(new Date());
+        builder.setExpiration(new Date((new Date()).getTime() + 1200000)); // 20 min
+        builder.signWith(SignatureAlgorithm.HS256, "secretkey");
+        return builder.compact();
     }
 }
