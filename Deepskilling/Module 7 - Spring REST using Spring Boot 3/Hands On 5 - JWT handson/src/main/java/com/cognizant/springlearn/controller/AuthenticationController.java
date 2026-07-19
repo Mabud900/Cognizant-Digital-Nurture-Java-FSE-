2683e9d45176b1b43b6977bfee31d999c118109f
md mabud <mdmabud.mail56@gmail.com>
2026-07-20 00:05:02 +0530

    (Module 7 - Spring REST using Spring Boot 3)Hands On- 5:Exercise 4 - Read Authorization header and decode the username and password

--- Deepskilling/Module 7 - Spring REST using Spring Boot 3/Hands On 5 - JWT handson/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ Deepskilling/Module 7 - Spring REST using Spring Boot 3/Hands On 5 - JWT handson/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,10 +17,38 @@ public class AuthenticationController {
     @RequestMapping("/authenticate")
     public Map<String,String> authenticate(@RequestHeader("Authorization") String authHeader){
         logger.info("Authentication Request Received");
-        logger.debug("authHeader {}",authHeader);
+        logger.debug("authHeader={}",authHeader);
+
+        String user=getUser(authHeader);
         Map<String,String>map=new HashMap<String,String>();
         map.put("token","");
         logger.info("Authentication response sent");
         return map;
+
+    }
+
+    private String getUser(String authHeader){
+
+        logger.debug("authHeader={}", authHeader);
+
+        String encodedCredential =
+                authHeader.replace("Basic ","");
+
+        logger.debug("encodedCredential={}", encodedCredential);
+
+        byte[] decodedBytes =
+                Base64.getDecoder().decode(encodedCredential);
+
+        String decodedCredential =
+                new String(decodedBytes);
+
+        logger.debug("decodedCredential={}", decodedCredential);
+
+        String user =
+                decodedCredential.substring(0, decodedCredential.indexOf(":"));
+
+        logger.debug("user={}", user);
+
+        return user;
     }
 }
